d62b5fdfeedf27751efaf934f411cee0555abcca
AlexanderJernstrom <alexanderjernstrom@gmail.com>
2021-02-28 16:28:47 +0100

    added build folder

---
+++ build/precache-manifest.4d21666d6048297d6d23f19a2592c7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b30793f54d920f680128e405f89f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e083eab596e1bb56b88",
+    "url": "/static/css/main.f1b80fd9.chunk.css"
+  },
+  {
+    "revision": "a44ef8d566d0167e3490",
+    "url": "/static/js/2.c828eda1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.c828eda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e083eab596e1bb56b88",
+    "url": "/static/js/main.14485169.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
